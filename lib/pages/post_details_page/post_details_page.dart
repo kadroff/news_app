e031c8ca9c4435139fdcb9c1cf63082c56edcd45
Marsel Kadrov <M.Kadrov@modulbank.ru>
2022-09-20 14:31:44 +0500

    refactoring ui components

--- lib/pages/post_details_page/post_details_page.dart
+++ lib/pages/post_details_page/post_details_page.dart
@@ -12,50 +12,49 @@ class PostDetailsPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Post details'),
       ),
-      body: Container(
+      body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            const SizedBox(height: 16),
-            Text(
-              post.title,
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-            ),
-            const SizedBox(height: 32),
-            post.description.isEmpty
-                ? const Text('Nothing yet...')
-                : Expanded(
-                    child: SingleChildScrollView(
-                      child: Text(
-                        post.description,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          color: Colors.grey[600],
-                        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 16),
+              Text(
+                post.title,
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(height: 32),
+              post.description.isEmpty
+                  ? const Text('Nothing yet...')
+                  : Text(
+                      post.description,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey[600],
                       ),
                     ),
+              const SizedBox(height: 32),
+              Row(
+                children: [
+                  Text(
+                    '${post.starsCounter}',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
                   ),
-            const SizedBox(height: 32),
-            Row(
-              children: [
-                Text(
-                  '${post.starsCounter}',
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black,
+                  const SizedBox(width: 4),
+                  const Icon(
+                    Icons.star_purple500_sharp,
+                    color: Colors.green,
+                    size: 24,
                   ),
-                ),
-                const SizedBox(width: 4),
-                const Icon(
-                  Icons.star_purple500_sharp,
-                  color: Colors.green,
-                  size: 24,
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-          ],
+                ],
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
         ),
       ),
     );
